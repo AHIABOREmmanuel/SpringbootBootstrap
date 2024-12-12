b0332be2a613ce8480f757229076949cdf31c533
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2024-12-12 09:37:02 +0000

    first commit

---
+++ src/main/java/com/springboot/bootstrap/controller/AccueilController.java
@@ -0,0 +1,15 @@
+package com.springboot.bootstrap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+@Controller
+public class AccueilController {
+    
+    @RequestMapping("/accueil")
+    public String accueilSubmit(){
+        return "index";
+    }
+    
+}*/
